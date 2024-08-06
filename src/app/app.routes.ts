1353e055ad4898057002835d263d393182aec505
ayssam <ayssam@entratus.ai>
2024-08-06 17:14:07 -0300

    Creating routes for the views

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './components/views/inicio/inicio.component';
+import { MissaoComponent } from './components/views/missao/missao.component';
+import { ServicosComponent } from './components/views/servicos/servicos.component';
+import { SobreNosComponent } from './components/views/sobre-nos/sobre-nos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: InicioComponent },
+    { path: 'missao', component: MissaoComponent },
+    { path: 'servicos', component: ServicosComponent },
+    { path: 'sobre-nos', component: SobreNosComponent }
+];
