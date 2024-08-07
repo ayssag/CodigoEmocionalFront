a25806d36625f069b436da35500ccc3ab8ab1a5d
ayssam <ayssam@entratus.ai>
2024-08-06 21:09:37 -0300

    Creating header component

--- src/app/components/views/inicio/inicio.component.ts
+++ src/app/components/views/inicio/inicio.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
+import { HeaderComponent } from '../../widgets/header/header.component';
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    HeaderComponent
+  ],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
-
+  screenWidth = window.innerWidth;
+  screenHeight = window.innerHeight;
 }
